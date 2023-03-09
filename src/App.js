9230b7ca336a0e67831d3092933fe08ce0b53b7e
Aniebietabasi Obo <85676508+anie308@users.noreply.github.com>
2023-03-09 00:18:49 +0000

    fixing route error with codespaces with mobile device

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import {useEffect, useState} from 'react'
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import NftLists from "./components/NftLists";
-import DetailsModal from './components/DetailsModal';
-import { getNfts } from './utils/getNfts';
-import Footer from './components/Footer';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
 function App() {
-  const [nfts, setNfts] = useState([]);
-
-  useEffect(() => {
-    //get all nfts
-    const fetchNfts = async () => {
-      const data = await getNfts();
-      if (data) {
-        setNfts(data);
-      }
-    };
-    fetchNfts();
-  }, []);
-  const [id, setId] = useState(null);
-  const [modal, setModal]= useState(false)
-  return (
-    <>
-    <div className="bg-primary relative min-h-screen">
-      <Navbar />
-      <Hero />
-      <NftLists nfts={nfts} setId={setId} setModal={setModal} modal={modal} />
-    
-      {
-        modal && (
-          <DetailsModal nfts={nfts} id={id} setModal={setModal}/>
-        )
-      }
-      <Footer/>
-    </div>
-    
-    </>
+return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { getNfts } from '../utils/getNfts'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import NftLists from '../components/NftLists'
+import DetailsModal from '../components/DetailsModal'
+import Footer from '../components/Footer'
+function Home() {
 
+  const [nfts, setNfts] = useState([]);
 
-function Homee(){
-    return (
-        <div></div>
-    )
+  useEffect(() => {
+    //get all nfts
+    const fetchNfts = async () => {
+      const data = await getNfts();
+      if (data) {
+        setNfts(data);
+      }
+    };
+    fetchNfts();
+  }, []);
+  const [id, setId] = useState(null);
+  const [modal, setModal] = useState(false)
+
+  return (
+    <div>
+      <div className="bg-primary relative min-h-screen">
+        <Navbar />
+        <Hero />
+        <NftLists nfts={nfts} setId={setId} setModal={setModal} modal={modal} />
+
+        {
+          modal && (
+            <DetailsModal nfts={nfts} id={id} setModal={setModal} />
+          )
+        }
+        <Footer />
+      </div>
+
+    </div>
+  )
 }
 
 export default Home;
